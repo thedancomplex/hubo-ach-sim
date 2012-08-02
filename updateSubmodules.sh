10575c6c4308714b5899c1612af7284739432736
Daniel M. Lofaro <geovana@hubo-ach-4ch.dhcp.drexel.edu>
2012-08-02 13:14:58 -0400

    modified what commit the open hubo is at in the updatesubmodule.sh

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,5 +1,6 @@
 git submodule init openHubo/
 git submodule update --recursive
 cd openHubo/
+git checkout c685c36c188d206c17bd0b3644e0cc10b6268733
 git submodule init huboJointConstants/
 git submodule update --recursive
