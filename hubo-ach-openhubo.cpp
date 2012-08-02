5940819e2389aa9002c10d380da488f7d0f8ca7b
Daniel M. Lofaro <geovana@hubo-ach-vm.(none)>
2012-08-02 06:03:42 -0400

    checking for colision once

--- hubo-ach-openhubo.cpp
+++ hubo-ach-openhubo.cpp
@@ -1,125 +1,107 @@
+/** \example orcollision.cpp
+    \author Rosen Diankov
+
+    Load a robot into the openrave environment, set it at [joint values] and check for self
+    collisions. Returns number of contact points.
+
+    Usage:
+    \verbatim
+    orcollision [--list] [--checker checker_name] [--joints #values [values]] body_model
+    \endverbatim
+
+    - \b --list - List all the loadable interfaces (ie, collision checkers).
+    - \b --checker - name Load a different collision checker instead of the default one.
+    - <b>--joints \#values [values]</b> - Set the robot to specific joint values
+
+    Example:
+    \verbatim
+    orcollision --checker ode robots/barrettwam.robot.xml
+    \endverbatim
+
+    <b>Full Example Code:</b>
+ */
 #include <openrave-core.h>
 #include <vector>
 #include <cstring>
 #include <sstream>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <boost/thread/thread.hpp>
-#include <boost/bind.hpp>
-
-
-#include <fcntl.h>
-#include <assert.h>
-#include <pthread.h>
-#include <stdbool.h>
-#include <math.h>
-#include <inttypes.h>
-#include "ach.h"
-
-#include "hubo.h"
-
-// ach message type
-typedef struct hubo huboOpen[1];
-
-// ach channels
-ach_channel_t chan_num;
-
-// Timing info
-#define NSEC_PER_SEC    1000000000
-
 
 using namespace OpenRAVE;
 using namespace std;
 
-static inline void tsnorm(struct timespec *ts){
-
-	while (ts->tv_nsec >= NSEC_PER_SEC) {
-		ts->tv_nsec -= NSEC_PER_SEC;
-		ts->tv_sec++;
-	}
+void printhelp()
+{
+    RAVELOG_INFO("orcollision [--list] [--checker checker_name] [--joints #values [values]] body_model\n");
 }
 
-int main( int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
 
-	printf("Start OpenHubo-Ach\n");
+    RaveInitialize(true); // start openrave core
+    EnvironmentBasePtr penv = RaveCreateEnvironment(); // create the main environment
+    vector<dReal> vsetvalues;
 
-	int hflag = 0;
-	int c;
+    // parse the command line options
+    int i = 1;
+    
 
-	struct timespec t;
-	int interval = 500000000; // 2hz (0.5 sec)
-	//int interval = 10000000; // 100 hz (0.01 sec)
-
-
-
-	huboOpen H;
-	// open ach channel
-	int r = ach_open(&chan_num, "hubo", NULL);
-	assert( ACH_OK == r );
-	size_t fs;
-	r = ach_get( &chan_num, H, sizeof(H), &fs, NULL, ACH_O_LAST );
-	assert( sizeof(H) == fs );
-//	assert( ACH_OK == r );
-/*
-	while ((c = getopt (argc, argv, "h")) != -1) {
-		switch (c) {
-			case 'h':
-				hflag = 1;
-				scenefilename = argv[1];
-				break;
-			default:
-				abort();
-		}
-	}
-
-*/
-
-	/* open openHubo */
-	EnvironmentBasePtr penv;
-	//string scenefilename = "openHubo/jaemiHubo.robot.xml";
-	string scenefilename = "openHubo/jaemiHubo.robot.xml";
-	RaveInitialize(true);
-	//string scenefilename = "data/lab1.env.xml";
-	//string viewername = "qtcoin";
-	try {
-	penv->Load(scenefilename);
-	printf("Loaded scene: \n");
-	}
-	catch(exception e) {
-		perror("load OpenRAVE ERR exiting");
-		return -1;
-	}
-//	vector<RobotBasePtr> vrobots;
-//	penv->GetRobots(vrobots);
-//	RobotBasePtr probot = vrobots.at(0);
-	
-//	ModuleBasePtr pbasemanip = RaveCreateModule(penv,"basemanipulation");
-
-
-	while(1) {
-
-
-                // wait until next shot
-                //clock_nanosleep(0,TIMER_ABSTIME,&t, NULL);
-		clock_nanosleep(0,TIMER_ABSTIME,&t, NULL);
+	// load the scene
+    if( !penv->Load("openHubo/jaemiHubo.robot.xml") ) {
+        return 2;
+    }
 
 
 
-		// get new hubo profile
-		r = ach_get( &chan_num, H, sizeof(H), &fs, NULL, ACH_O_LAST );
-		assert( sizeof(H) == fs );
 
-		// do stuff here
+    int contactpoints = 0;
+    {
+        // lock the environment to prevent data from changing
+        EnvironmentMutex::scoped_lock lock(penv->GetMutex());
 
-		t.tv_nsec+=interval;
-		tsnorm(&t);
-	}
+        vector<KinBodyPtr> vbodies;
+        penv->GetBodies(vbodies);
+        // get the first body
+        if( vbodies.size() == 0 ) {
+            RAVELOG_ERROR("no bodies loaded\n");
+            return -3;
+        }
 
-//	printf("%s",scenefilename);
+        KinBodyPtr pbody = vbodies.at(0);
+        vector<dReal> values;
+        pbody->GetDOFValues(values);
 
+        // set new values
+        for(int i = 0; i < (int)vsetvalues.size() && i < (int)values.size(); ++i) {
+            values[i] = vsetvalues[i];
+        }
+        pbody->SetDOFValues(values,true);
+
+        CollisionReportPtr report(new CollisionReport());
+        penv->GetCollisionChecker()->SetCollisionOptions(CO_Contacts);
+        if( pbody->CheckSelfCollision(report) ) {
+            contactpoints = (int)report->contacts.size();
+            stringstream ss;
+            ss << "body in self-collision "
+               << (!!report->plink1 ? report->plink1->GetName() : "") << ":"
+               << (!!report->plink2 ? report->plink2->GetName() : "") << " at "
+               << contactpoints << "contacts" << endl;
+            for(int i = 0; i < contactpoints; ++i) {
+                CollisionReport::CONTACT& c = report->contacts[i];
+                ss << "contact" << i << ": pos=("
+                   << c.pos.x << ", " << c.pos.y << ", " << c.pos.z << "), norm=("
+                   << c.norm.x << ", " << c.norm.y << ", " << c.norm.z << ")" << endl;
+            }
+
+            RAVELOG_INFOA(ss.str());
+        }
+        else {
+            RAVELOG_INFO("body not in collision\n");
+        }
+
+        // get the transformations of all the links
+        vector<Transform> vlinktransforms;
+        pbody->GetLinkTransformations(vlinktransforms);
+    }
+
+    RaveDestroy(); // destroy
+    return contactpoints;
 }
-
-
